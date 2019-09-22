4b22821c7909ca386cc2e562f66c83f9f3b2fdde
Timo Friman <tfriman@redhat.com>
2019-09-22 11:59:11 +0300

    Added demo scripts

---
+++ deploy-script.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+. ./demo-magic/demo-magic.sh
+
+clear
+
+p "mvn clean package -Pnative -Dnative-image.docker-build=true -DskipTests=true"
+
+pe "oc new-project forum-quarkus --display-name="ForumQuarkus""
+pe "oc new-build quay.io/redhat/ubi-quarkus-native-runner --binary --name=forum-quarkus -l app=forum-quarkus"
+pe "oc start-build forum-quarkus --from-file=target/quarkus-forum-1.0-SNAPSHOT-runner --follow"
+pe "oc new-app forum-quarkus"
+pe "oc expose service forum-quarkus"
+pe "oc set resources dc/forum-quarkus --limits=memory=50Mi "
+pe "oc scale --replicas=10 dc/forum-quarkus"
